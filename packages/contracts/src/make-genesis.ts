a4bbf05a6639542f9268ca0a2da950c3dff79e9a
kf <kelvinfichter@gmail.com>
2021-09-04 06:35:52 +0000

    refactor: remove custom genesis logic

--- packages/contracts/src/make-genesis.ts
+++ packages/contracts/src/make-genesis.ts
@@ -1,25 +1,34 @@
 /* External Imports */
-import { Signer } from 'ethers'
 import {
   computeStorageSlots,
   getStorageLayout,
 } from '@defi-wonderland/smock/dist/src/utils'
+import { remove0x } from '@eth-optimism/core-utils'
 
 /* Internal Imports */
 import { predeploys } from './predeploys'
 import { getContractArtifact } from './contract-artifacts'
 
 export interface RollupDeployConfig {
-  whitelistConfig: {
-    owner: string | Signer
-    allowArbitraryContractDeployment: boolean
-  }
-  gasPriceOracleConfig: {
-    owner: string | Signer
-    initialGasPrice: number
-  }
+  // Address that will own the L2 deployer whitelist.
+  whitelistOwner: string
+  // Whether or not to allow arbitrary contract deployment.
+  allowArbitraryContractDeployment: boolean
+  // Address that will own the L2 gas price oracle.
+  gasPriceOracleOwner: string
+  // Initial value for the L2 gas price.
+  initialGasPrice: number
+  // Initial value for the L2 block gas limit.
+  l2BlockGasLimit: number
+  // Chain ID to give the L2 network.
+  l2ChainId: number
+  // Address of the key that will sign blocks.
+  blockSignerAddress: string
+  // Address of the L1StandardBridge contract.
   l1StandardBridgeAddress: string
+  // Address of the L1 fee wallet.
   l1FeeWalletAddress: string
+  // Address of the L1CrossDomainMessenger contract.
   l1CrossDomainMessengerAddress: string
 }
 
@@ -29,17 +38,25 @@ export interface RollupDeployConfig {
  * @param cfg Configuration for the L2 system.
  * @returns Generated L2 genesis state.
  */
-export const makeStateDump = async (cfg: RollupDeployConfig): Promise<any> => {
+export const makeL2GenesisFile = async (
+  cfg: RollupDeployConfig
+): Promise<any> => {
+  // Very basic validation.
+  for (const [key, val] of Object.entries(cfg)) {
+    if (val === undefined) {
+      throw new Error(`must provide an input for config value: ${key}`)
+    }
+  }
+
   const variables = {
     OVM_DeployerWhitelist: {
       initialized: true,
-      allowArbitraryDeployment:
-        cfg.whitelistConfig.allowArbitraryContractDeployment,
-      owner: cfg.whitelistConfig.owner,
+      allowArbitraryDeployment: cfg.allowArbitraryContractDeployment,
+      owner: cfg.whitelistOwner,
     },
     OVM_GasPriceOracle: {
-      _owner: cfg.gasPriceOracleConfig.owner,
-      gasPrice: cfg.gasPriceOracleConfig.initialGasPrice,
+      _owner: cfg.gasPriceOracleOwner,
+      gasPrice: cfg.initialGasPrice,
     },
     OVM_L2StandardBridge: {
       l1TokenBridge: cfg.l1StandardBridgeAddress,
@@ -89,5 +106,30 @@ export const makeStateDump = async (cfg: RollupDeployConfig): Promise<any> => {
     }
   }
 
-  return dump
+  return {
+    config: {
+      chainId: cfg.l2ChainId,
+      homesteadBlock: 0,
+      eip150Block: 0,
+      eip155Block: 0,
+      eip158Block: 0,
+      byzantiumBlock: 0,
+      constantinopleBlock: 0,
+      petersburgBlock: 0,
+      istanbulBlock: 0,
+      muirGlacierBlock: 0,
+      clique: {
+        period: 0,
+        epoch: 30000,
+      },
+    },
+    difficulty: '1',
+    gasLimit: cfg.l2BlockGasLimit.toString(10),
+    extradata:
+      '0x' +
+      '00'.repeat(32) +
+      remove0x(cfg.blockSignerAddress) +
+      '00'.repeat(65),
+    alloc: dump,
+  }
 }
